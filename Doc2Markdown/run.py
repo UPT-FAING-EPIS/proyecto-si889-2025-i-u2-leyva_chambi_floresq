510c9406bfc8794b5b6e4f8a817ee499005503a3
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-05-31 11:46:19 -0500

    feat: actualiza funcionalidad X

--- Doc2Markdown/run.py
+++ Doc2Markdown/run.py
@@ -1,3 +1,14 @@
+#import uvicorn
+#from app.app import app
+
+#if _name_ == "_main_":
+#    uvicorn.run(
+#    "app.app:app",
+#    host="0.0.0.0",  # Cambiar a 0.0.0.0 en producción
+#    port=8000,
+#    reload=False  # Deshabilitar recarga en producción
+#)
+    
 # CONFIGURACION LOCAL
 import uvicorn
 from app.app import app
