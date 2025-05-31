d0312cfd25c424e05a6251e3ba93f0cfe1f297e2
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-05-31 09:29:51 -0500

    Version 12 - workflow deploy

--- Doc2Markdown/run.py
+++ Doc2Markdown/run.py
@@ -1,24 +1,15 @@
-#import uvicorn
-#from app.app import app
-
-#if __name__ == "__main__":
-#    uvicorn.run(
-#    "app.app:app",
-#    host="0.0.0.0",  # Cambiar a 0.0.0.0 en producción
-#    port=8000,
-#    reload=False  # Deshabilitar recarga en producción
-#)
-    
-# CONFIGURACION LOCAL
-import uvicorn
-from app.app import app
+# run.py
+import os
+import subprocess
 
 if __name__ == "__main__":
-    uvicorn.run(
+    port = os.environ.get("PORT", "8000")
+    cmd = [
+        "gunicorn", 
+        "-w", "4", 
+        "-k", "uvicorn.workers.UvicornWorker",
         "app.app:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
-        #ssl_keyfile="./key.pem",
-        #ssl_certfile="./cert.pem"
-    )    
+        "--bind", f"0.0.0.0:{port}",
+        "--timeout", "600"
+    ]
+    subprocess.run(cmd)
--- Doc2Markdown/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/site/wwwroot
-
-exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.app:app --bind 0.0.0.0:8000 --timeout 600
