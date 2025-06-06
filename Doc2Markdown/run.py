de69fa588d1f8e055575fd7d3c2c8f9f1add0a10
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-06-05 22:44:21 -0500

    Test

--- Doc2Markdown/run.py
+++ Doc2Markdown/run.py
@@ -1,24 +1,25 @@
-#import uvicorn
-#from app.app import app
-
-#if _name_ == "_main_":
-#    uvicorn.run(
-#    "app.app:app",
-#    host="0.0.0.0",  # Cambiar a 0.0.0.0 en producción
-#    port=8000,
-#    reload=False  # Deshabilitar recarga en producción
-#)
-    
-# CONFIGURACION LOCAL ACTUALIZADA 2
+# CONFIGURACION DE PRODUCCION ACTUALIZADA 
 import uvicorn
 from app.app import app
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.app:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
-        reload=True
-        #ssl_keyfile="./key.pem",
-        #ssl_certfile="./cert.pem"
+        reload=False
     )
+
+# CONFIGURACION LOCAL ACTUALIZADA 
+#import uvicorn
+#from app.app import app
+
+#if __name__ == "__main__":
+#    uvicorn.run(
+#        "app.app:app",
+#        host="127.0.0.1",
+#        port=8000,
+#        reload=True
+#        #ssl_keyfile="./key.pem",
+#        #ssl_certfile="./cert.pem"
+#    )
